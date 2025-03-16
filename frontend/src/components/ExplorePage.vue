81017124cfeab1bd667e97ae9d530ebd0956112c
meneerezra <ezraschutte227@gmail.com>
2025-03-16 12:54:28 +0100

    Feat: Added test button to frontend

--- frontend/src/components/ExplorePage.vue
+++ frontend/src/components/ExplorePage.vue
@@ -2,21 +2,34 @@
     <div class="page">
       <h1>Explore</h1>
       <p>Discover new content here.</p>
+      <button @click="real()">bruh button</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
+
+</template>
+
+<script setup>
+import {RealDebridClient} from "../../bindings/derpy-launcher072/torrent/realdebrid/index.js";
+
+function real() {
+    RealDebridClient.GetTrafic().then((result) => {
+        console.log(result)
+    })
+}
+</script>
+
+<script>
+export default {
     name: 'ExplorePage'
-  }
-  </script>
-  
-  <style scoped>
-  .page {
+}
+
+</script>
+
+<style scoped>
+.page {
     padding: 20px;
-  }
-  
-  h1 {
+}
+
+h1 {
     margin-bottom: 15px;
-  }
-  </style>
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import { AddToLibrary } from '../bindings/derpy-launcher072/library/library'
+import {RealDebridClient} from "../bindings/derpy-launcher072/torrent/realdebrid/index.js";
 
 //AddToLibrary(69)
 
