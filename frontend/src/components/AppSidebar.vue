8907b1b59604d1eb48c7ddf65e40386d6b49e9ad
Dominik <p.dominikkrakowski04@gmail.com>
2025-03-18 13:52:24 +0100

    FEAT: Sidebar redesign, active page indicator and color changes

--- frontend/src/components/AppSidebar.vue
+++ frontend/src/components/AppSidebar.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="sidebar">
     <div class="sidebar-content">
-      <router-link to="/explore" custom v-slot="{ navigate }">
-        <button @click="navigate">Explore</button>
+      <router-link to="/explore" custom v-slot="{ navigate, isActive }">
+        <button @click="navigate" class="nav-button">
+          <div v-if="isActive" class="active-indicator-sidebar"></div>
+          Explore
+        </button>
       </router-link>
-      <router-link to="/library" custom v-slot="{ navigate }">
-        <button @click="navigate">Library</button>
+      <router-link to="/library" custom v-slot="{ navigate, isActive }">
+        <button @click="navigate" class="nav-button">
+          <div v-if="isActive" class="active-indicator-sidebar"></div>
+          Library
+        </button>
       </router-link>
     </div>
   </div>
@@ -20,10 +26,34 @@ export default {
 <style scoped>
 .sidebar {
   width: 200px;
+  padding: 0 20px;
   height: calc(100% - 40px);
-  background-color: rgb(68, 48, 48);
+  background-color: rgb(22, 22, 22);
   position: fixed;
   bottom: 0;
+}
+
+.active-indicator-sidebar {
+  width: 4px;
+  height: 0;
+  background-color: var(--accent-color);
+  box-shadow: 0px 0px 20px var(--accent-color);
+  border-radius: 50px;
+  position: absolute;
+  left: 0;
+  margin-left: -10px;
+  top: 50%;
+  transform: translateY(-50%);
+  animation: growHeight 0.3s ease forwards;
+}
+
+@keyframes growHeight {
+  from {
+    height: 0;
+  }
+  to {
+    height: 35px;
+  }
 }
 
 .sidebar-content {
@@ -34,18 +64,20 @@ export default {
   flex-direction: column;
 }
 
-.sidebar-content button {
+.nav-button {
+  position: relative;
   color: white;
   background: none;
   text-decoration: none;
   border: none;
-  padding: 10px;
+  padding: 15px 20px;
   text-align: left;
+  border-radius: 10px;
   transition: background-color 0.3s ease;
   cursor: pointer;
 }
 
-.sidebar-content button:hover {
-  background-color: rgba(255, 255, 255, 0.164);
+.nav-button:hover {
+  background-color: rgba(255, 255, 255, 0.025);
 }
 </style>
