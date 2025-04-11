ce46431a92b53554537c9b65450ce9451b2f9e52
meneerezra <ezraschutte227@gmail.com>
2025-04-11 23:06:04 +0200

    Fix: Game library doesnt blow up anymore

--- main.go
+++ main.go
@@ -88,10 +88,7 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	if settings.UseCaching {
-		library.StartImageServer()
-		go libraryManager.CheckForCache()
-	}
+
 
 	if settings.RealDebridSettings.UseRealDebrid {
 		if settings.RealDebridSettings.DebridToken == "" {
@@ -247,6 +244,11 @@ func main() {
 		}
 
 	}()*/
+
+	if settings.UseCaching {
+		library.StartImageServer()
+		go libraryManager.CheckForCache()
+	}
 
 	// Complete aot season 4
 	queue.AddRealDebridDownloadToQueue("magnet:?xt=urn:btih:ac8dc037d282f82efb2864abdd54399029105c0c&dn=%5BGolumpa-Yameii%5D%20Attack%20on%20Titan%20-%20The%20Final%20Season%20%5BEnglish%20Dub%5D%20%5BWEB-DL%20720p%5D%20-%20%28The%20Complete%20S04%29%20-%20Unofficial%20Batch&tr=http%3A%2F%2Fnyaa.tracker.wf%3A7777%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce")
